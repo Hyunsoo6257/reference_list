341405fa822ec176fb39736baa723fe3d53fee0d
Molly <162527916+Hyunsoo6257@users.noreply.github.com>
2024-09-09 08:54:41 +1000

    add env

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDlgH9AG63F0wCKkqT-7bGJ2OC9U7feII",
-  authDomain: "referee-8cc26.firebaseapp.com",
-  projectId: "referee-8cc26",
-  storageBucket: "referee-8cc26.appspot.com",
-  messagingSenderId: "950616561967",
-  appId: "1:950616561967:web:4e3bda46820efc06d829da",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
